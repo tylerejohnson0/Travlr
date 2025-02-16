8e53f470a7fefc13429d1fa331953ae251e932d5
tylerejohnson0 <66090295+tylerejohnson0@users.noreply.github.com>
2025-02-16 14:40:02 -0600

    Module 5

--- app.js
+++ app.js
@@ -1,49 +1,68 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');
-var handlebars = require('hbs');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var handlebars = require("hbs");
 
+
+require("./app_api/models/db");
+
+var indexRouter = require("./app_server/routes/index");
+var usersRouter = require("./app_server/routes/users");
+var travelRouter = require("./app_server/routes/travel");
+var apiRouter = require("./app_api/routes/index");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app-server', 'views'));
+app.set("views", path.join(__dirname, "app_server", "views"));
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+handlebars.registerPartials(__dirname + "/app_server/views/partials");
+app.set("view engine", "hbs");
 
-handlebars.registerPartials(__dirname + '/app_server/views/partials')
-
-app.set('view engine', 'hbs');
-
-
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/travel', travelRouter)
+// Allow CORS
+app.use("/api", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/travel", travelRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
+// Catch unauthorized error and create 401
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ message: err.name + ": " + err.message });
+  }
+});
 module.exports = app;
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/travel');
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/travel");
 
-router.get('/', controller.travel);
+router.get("/", controller.travel);
 
 module.exports = router;
